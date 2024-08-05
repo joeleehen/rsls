e35efe39831bdbb73fe8460b421a848d8827c33c
joseph <joeylhendrix@gmail.com>
2024-08-05 17:20:35 -0500

    added icons and colors

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,28 @@ const RED: &str = "\x1b[31m";
 const YELLOW: &str = "\x1b[33m";
 const BLUE: &str = "\x1b[34m";
 const MAGENTA: &str = "\x1b[35m";
+const WHITE: &str = "\x1b[97m";
+const CYAN: &str = "\x1b[36m";
+const ORANGE: &str = "\x1b[38;5;208m";
+const PURPLE: &str = "\x1b[35m";
+const GRAY: &str = "\x1b[37m";
+const LIGHTRED: &str = "\x1b[91m";
+const LIGHTGREEN: &str = "\x1b[92m";
+const LIGHTYELLOW: &str = "\x1b[93m";
+const LIGHTBLUE: &str = "\x1b[94m";
+const LIGHTPURPLE: &str = "\x1b[95m";
+const LIGHTCYAN: &str = "\x1b[38;5;87m";
+const DARKGREEN: &str = "\x1b[38;5;22m";
+const DARKORANGE: &str = "\x1b[38;5;208m";
+const DARKYELLOW: &str = "\x1b[38;5;172m";
+const DARKMAGENTA: &str = "\x1b[38;5;125m";
+const DARKGRAY: &str = "\x1b[90m";
+const BRIGHTRED: &str = "\x1b[38;5;196m";
+const BRIGHTGREEN: &str = "\x1b[38;5;46m";
+const BRIGHTYELLOW: &str = "\x1b[38;5;226m";
+const BRIGHTBLUE: &str = "\x1b[38;5;39m";
+const BRIGHTMAGENTA: &str = "\x1b[38;5;198m";
+const BRIGHTCYAN: &str = "\x1b[38;5;51m";
 
 fn parse_permissions(mode: u32) -> String {
     let user = triplet(mode, S_IRUSR as u32, S_IWUSR as u32, S_IXUSR as u32);
@@ -70,7 +92,7 @@ fn main() {
             process::exit(1);
         }
     } else {
-        if let Err(ref e) = run(include_hidden, force_columns, &args.dir) {
+        if let Err(ref e) = run(include_hidden, force_columns, file_icons, &args.dir) {
             println!("{}", e);
             process::exit(1);
         }
@@ -91,99 +113,159 @@ fn append_icon(file_name: String) -> String {
     }
 }
 
-fn create_icon_hashmap() -> HashMap<&'static str, &'static str> {
+fn create_icon_hashmap() -> HashMap<String, &'static str> {
     let mut file_icons = HashMap::new();
-    file_icons.insert(".go", " ");
-    file_icons.insert(".mod", " ");
-    file_icons.insert(".sh"," ");
-    file_icons.insert(".cpp"," ");
-    file_icons.insert(".hpp"," ");
-    file_icons.insert(".cxx"," ");
-    file_icons.insert(".hxx"," ");
-    file_icons.insert(".css"," ");
-    file_icons.insert(".c"," ");
-    file_icons.insert("h"," ");
-    file_icons.insert(".cs","󰌛 ");
-    file_icons.insert(".png"," ");
-    file_icons.insert(".jpg"," ");
-    file_icons.insert(".JPG"," ");
-    file_icons.insert(".jpeg"," ");
-    file_icons.insert(".webp"," ");
-    file_icons.insert(".xcf"," ");
-    file_icons.insert(".xml","󰗀 ");
-    file_icons.insert(".htm"," ");
-    file_icons.insert(".html"," ");
-    file_icons.insert(".txt"," ");
-    file_icons.insert(".mp3"," ");
-    file_icons.insert(".m4a"," ");
-    file_icons.insert(".ogg"," ");
-    file_icons.insert(".flac"," ");
-    file_icons.insert(".mp4"," ");
-    file_icons.insert(".mkv"," ");
-    file_icons.insert(".webm"," ");
-    file_icons.insert(".zip","󰿺 ");
-    file_icons.insert(".tar","󰛫 ");
-    file_icons.insert(".gz","󰛫 ");
-    file_icons.insert(".bz2","󰿺 ");
-    file_icons.insert(".xz","󰿺 ");
-    file_icons.insert(".jar"," ");
-    file_icons.insert(".java"," ");
-    file_icons.insert(".js"," ");
-    file_icons.insert(".json"," ");
-    file_icons.insert(".py"," ");
-    file_icons.insert(".rs"," ");
-    file_icons.insert(".yml"," ");
-    file_icons.insert(".yaml"," ");
-    file_icons.insert(".toml"," ");
-    file_icons.insert(".deb"," ");
-    file_icons.insert(".md"," ");
-    file_icons.insert(".rb"," ");
-    file_icons.insert(".php"," ");
-    file_icons.insert(".pl"," ");
-    file_icons.insert(".svg","󰜡 ");
-    file_icons.insert(".eps"," ");
-    file_icons.insert(".ps"," ");
-    file_icons.insert(".git"," ");
-    file_icons.insert(".zig"," ");
-    file_icons.insert(".xbps"," ");
-    file_icons.insert(".el"," ");
-    file_icons.insert(".vim"," ");
-    file_icons.insert(".lua"," ");
-    file_icons.insert(".pdf"," ");
-    file_icons.insert(".epub","󰂺 ");
-    file_icons.insert(".conf"," ");
-    file_icons.insert(".iso"," ");
-    file_icons.insert(".exe"," ");
-    file_icons.insert(".odt","󰷈 ");
-    file_icons.insert(".ods","󰱾 ");
-    file_icons.insert(".odp","󰈧 ");
-    file_icons.insert(".gif","󰵸 ");
-    file_icons.insert(".tiff","󰋪 ");
-    file_icons.insert(".7z"," ");
-    file_icons.insert(".bat"," ");
-    file_icons.insert(".app"," ");
-    file_icons.insert(".log"," ");
-    file_icons.insert(".sql"," ");
-    file_icons.insert(".db"," ");
+    file_icons.insert("go".to_string(), " ");
+    file_icons.insert("mod".to_string(), " ");
+    file_icons.insert("sh".to_string()," ");
+    file_icons.insert("cpp".to_string()," ");
+    file_icons.insert("hpp".to_string()," ");
+    file_icons.insert("cxx".to_string()," ");
+    file_icons.insert("hxx".to_string()," ");
+    file_icons.insert("css".to_string()," ");
+    file_icons.insert("c".to_string()," ");
+    file_icons.insert("h".to_string()," ");
+    file_icons.insert("cs".to_string(),"󰌛 ");
+    file_icons.insert("png".to_string()," ");
+    file_icons.insert("jpg".to_string()," ");
+    file_icons.insert("JPG".to_string()," ");
+    file_icons.insert("jpeg".to_string()," ");
+    file_icons.insert("webp".to_string()," ");
+    file_icons.insert("xcf".to_string()," ");
+    file_icons.insert("xml".to_string(),"󰗀 ");
+    file_icons.insert("htm".to_string()," ");
+    file_icons.insert("html".to_string()," ");
+    file_icons.insert("txt".to_string()," ");
+    file_icons.insert("mp3".to_string()," ");
+    file_icons.insert("m4a".to_string()," ");
+    file_icons.insert("ogg".to_string()," ");
+    file_icons.insert("flac".to_string()," ");
+    file_icons.insert("mp4".to_string()," ");
+    file_icons.insert("mkv".to_string()," ");
+    file_icons.insert("webm".to_string()," ");
+    file_icons.insert("zip".to_string(),"󰿺 ");
+    file_icons.insert("tar".to_string(),"󰛫 ");
+    file_icons.insert("gz".to_string(),"󰛫 ");
+    file_icons.insert("bz2".to_string(),"󰿺 ");
+    file_icons.insert("xz".to_string(),"󰿺 ");
+    file_icons.insert("jar".to_string()," ");
+    file_icons.insert("java".to_string()," ");
+    file_icons.insert("js".to_string()," ");
+    file_icons.insert("json".to_string()," ");
+    file_icons.insert("py".to_string()," ");
+    file_icons.insert("rs".to_string()," ");
+    file_icons.insert("yml".to_string()," ");
+    file_icons.insert("yaml".to_string()," ");
+    file_icons.insert("toml".to_string()," ");
+    file_icons.insert("deb".to_string()," ");
+    file_icons.insert("md".to_string()," ");
+    file_icons.insert("rb".to_string()," ");
+    file_icons.insert("php".to_string()," ");
+    file_icons.insert("pl".to_string()," ");
+    file_icons.insert("svg".to_string(),"󰜡 ");
+    file_icons.insert("eps".to_string()," ");
+    file_icons.insert("ps".to_string()," ");
+    file_icons.insert("git".to_string()," ");
+    file_icons.insert("zig".to_string()," ");
+    file_icons.insert("xbps".to_string()," ");
+    file_icons.insert("el".to_string()," ");
+    file_icons.insert("vim".to_string()," ");
+    file_icons.insert("lua".to_string()," ");
+    file_icons.insert("pdf".to_string()," ");
+    file_icons.insert("epub".to_string(),"󰂺 ");
+    file_icons.insert("conf".to_string()," ");
+    file_icons.insert("iso".to_string()," ");
+    file_icons.insert("exe".to_string()," ");
+    file_icons.insert("odt".to_string(),"󰷈 ");
+    file_icons.insert("ods".to_string(),"󰱾 ");
+    file_icons.insert("odp".to_string(),"󰈧 ");
+    file_icons.insert("gif".to_string(),"󰵸 ");
+    file_icons.insert("tiff".to_string(),"󰋪 ");
+    file_icons.insert("7z".to_string()," ");
+    file_icons.insert("bat".to_string()," ");
+    file_icons.insert("app".to_string()," ");
+    file_icons.insert("log".to_string()," ");
+    file_icons.insert("sql".to_string()," ");
+    file_icons.insert("db".to_string()," ");
 
     file_icons
 }
 
-fn output_to_term(mut files: Vec<String>, force_col: bool, longest_file_name: usize) {
+fn output_to_term(mut files: Vec<String>, force_col: bool, longest_file_name: usize, file_icons: HashMap<String, &str>) {
     files.sort();
     let ncol = termsize::get().unwrap().cols / (4 + longest_file_name as u16);
     let nrow = files.len() as u16 / ncol;
     let total_files = files.len();
-    println!("total files: {total_files}    nrows: {nrow}    ncol: {ncol}");
 
     let mut n = 0;
     for entry in files {
-        // TODO print file here, check for column output after
         let split_name = &entry.split('/').collect::<Vec<&str>>();
+
+        // printing directories
         if split_name.len() == 2 && split_name[1] == "" {
             print!("{BLUE} {entry}{RESET}");
+
+        // printing files
         } else {
             // TODO: if icon isn't found append two empty space
+            let split_name = &entry.split('.').collect::<Vec<&str>>();
+            if split_name.len() != 2 {
+                print!("{entry}  ");
+            }
+
+            let extension = split_name[1];
+            //if file_icons.contains_key(&extension) {
+            //    let icon = file_icons.get(&extension);
+            //    print!("{icon}");
+            //}
+            let icon = file_icons.get(extension);
+            if icon.is_some() {
+                let icon = icon.unwrap().to_string();
+                match extension {
+                    "go" => print!("{CYAN}{icon}{RESET}"),
+                    "sh" => print!("{BRIGHTGREEN}{icon}{RESET}"),
+                    "cpp"|"hpp"|"cxx"|"hxx" => print!("{BLUE}{icon}{RESET}"),
+                    "css" => print!("{LIGHTBLUE}{icon}{RESET}"),
+                    "c"|"h" => print!("{BLUE}{icon}{RESET}"),
+                    "cs" => print!("{DARKMAGENTA}{icon}{RESET}"),
+                    "png"|"jpg"|"jpeg"|"JPG"|"webp" => print!("{BRIGHTMAGENTA}{icon}{RESET}"),
+                    "gif" => print!("{MAGENTA}{icon}{RESET}"),
+                    "xcf" => print!("{PURPLE}{icon}{RESET}"),
+                    "xml" => print!("{LIGHTCYAN}{icon}{RESET}"),
+                    "htm"|"html" => print!("{ORANGE}{icon}{RESET}"),
+                    "txt"|"app" => print!("{WHITE}{icon}{RESET}"),
+                    "mp3"|"m4a"|"ogg"|"flac" => print!("{BRIGHTBLUE}{icon}{RESET}"),
+                    "mp4"|"mkv"|"webm" => print!("{BRIGHTMAGENTA}{icon}{RESET}"),
+                    "zip"|"tar"|"gz"|"bz2"|"xz"|"7z" => print!("{LIGHTPURPLE}{icon}{RESET}"),
+                    "jar"|"java" => print!("{ORANGE}{icon}{RESET}"),
+                    "js" => print!("{YELLOW}{icon}{RESET}"),
+                    "json"|"tiff" => print!("{BRIGHTYELLOW}{icon}{RESET}"),
+                    ".y" => print!("{DARKYELLOW}{icon}{RESET}"),
+                    "rs" => print!("{DARKGRAY}{icon}{RESET}"),
+                    "yml"|"yaml" => print!("{BRIGHTRED}{icon}{RESET}"),
+                    "toml" => print!("{DARKORANGE}{icon}{RESET}"),
+                    "deb" => print!("{LIGHTRED}{icon}{RESET}"),
+                    "md" => print!("{CYAN}{icon}{RESET}"),
+                    "rb" => print!("{RED}{icon}{RESET}"),
+                    "php" => print!("{BRIGHTBLUE}{icon}{RESET}"),
+                    "pl" => print!("{RED}{icon}{RESET}"),
+                    "svg" => print!("{LIGHTPURPLE}{icon}{RESET}"),
+                    "eps"|"ps" => print!("{ORANGE}{icon}{RESET}"),
+                    "git" => print!("{ORANGE}{icon}{RESET}"),
+                    "zig" => print!("{DARKORANGE}{icon}{RESET}"),
+                    "xbps" => print!("{DARKGREEN}{icon}{RESET}"),
+                    "el" => print!("{PURPLE}{icon}{RESET}"),
+                    "vim" => print!("{DARKGREEN}{icon}{RESET}"),
+                    "lua"|"sql" => print!("{BRIGHTBLUE}{icon}{RESET}"),
+                    "pdf"|"db" => print!("{BRIGHTRED}{icon}{RESET}"),
+                    "epub" => print!("{CYAN}{icon}{RESET}"),
+                    "conf"|"bat" => print!("{DARKGRAY}{icon}{RESET}"),
+                    "iso" => print!("{GRAY}{icon}{RESET}"),
+                    "exe" => print!("{BRIGHTCYAN}{icon}{RESET}"),
+                    _ => print!("")
+                }
+                print!("{entry}");
+            }
             print!("{entry}  ");
         }
         if force_col {
@@ -201,8 +283,7 @@ fn output_to_term(mut files: Vec<String>, force_col: bool, longest_file_name: us
     }
 }
 
-fn run(include_hidden: bool, force_col: bool, dir: &PathBuf) -> Result<(), Box<dyn Error>> {
-    //let mut files = Vec::new();
+fn run(include_hidden: bool, force_col: bool, file_icons: HashMap<String, &str>, dir: &PathBuf) -> Result<(), Box<dyn Error>> {
     if dir.is_dir() {
         let mut files = Vec::new();
         let mut longest_file_name = 0;
@@ -219,19 +300,17 @@ fn run(include_hidden: bool, force_col: bool, dir: &PathBuf) -> Result<(), Box<d
             if !include_hidden {
                 // skip hidden files
                 if file_name.chars().nth(0) != Some('.') {
-                    // println!("{}", file_name);
                     longest_file_name =
                         cmp::max(longest_file_name as usize, file_name.chars().count());
                     files.push(file_name);
                 }
             } else {
                 // include hidden files
-                // println!("{}", file_name);
                 longest_file_name = cmp::max(longest_file_name as usize, file_name.len());
                 files.push(file_name);
             }
         }
-        output_to_term(files, force_col, longest_file_name);
+        output_to_term(files, force_col, longest_file_name, file_icons);
         println!("");
     }
     Ok(())
