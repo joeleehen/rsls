5b0a03fbeae2ff2dd20aec99d11c91a0e17d58eb
joseph <joeylhendrix@gmail.com>
2024-08-05 15:38:17 -0500

    file icon prep

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// FIXME: my newlines are fucked up now after formatting changes for some reason
 mod args;
 
 use args::RsArgs;
@@ -21,6 +20,7 @@ use humansize::{format_size, DECIMAL};
 
 use libc::{S_IRGRP, S_IROTH, S_IRUSR, S_IWGRP, S_IWOTH, S_IWUSR, S_IXGRP, S_IXOTH, S_IXUSR};
 use std::os::unix::fs::PermissionsExt;
+use std::collections::HashMap;
 
 const RESET: &str = "\x1b[0m";
 const GREEN: &str = "\x1b[32m";
@@ -53,6 +53,7 @@ fn triplet(mode: u32, read: u32, write: u32, execute: u32) -> String {
 fn main() {
     let mut include_hidden = false;
     let mut force_columns = false;
+    let file_icons = create_icon_hashmap();
     let args: RsArgs = RsArgs::parse();
 
     if args.all {
@@ -90,34 +91,111 @@ fn append_icon(file_name: String) -> String {
     }
 }
 
+fn create_icon_hashmap() -> HashMap<&'static str, &'static str> {
+    let mut file_icons = HashMap::new();
+    file_icons.insert(".go", " ");
+    file_icons.insert(".mod", " ");
+    file_icons.insert(".sh"," ");
+    file_icons.insert(".cpp"," ");
+    file_icons.insert(".hpp"," ");
+    file_icons.insert(".cxx"," ");
+    file_icons.insert(".hxx"," ");
+    file_icons.insert(".css"," ");
+    file_icons.insert(".c"," ");
+    file_icons.insert("h"," ");
+    file_icons.insert(".cs","󰌛 ");
+    file_icons.insert(".png"," ");
+    file_icons.insert(".jpg"," ");
+    file_icons.insert(".JPG"," ");
+    file_icons.insert(".jpeg"," ");
+    file_icons.insert(".webp"," ");
+    file_icons.insert(".xcf"," ");
+    file_icons.insert(".xml","󰗀 ");
+    file_icons.insert(".htm"," ");
+    file_icons.insert(".html"," ");
+    file_icons.insert(".txt"," ");
+    file_icons.insert(".mp3"," ");
+    file_icons.insert(".m4a"," ");
+    file_icons.insert(".ogg"," ");
+    file_icons.insert(".flac"," ");
+    file_icons.insert(".mp4"," ");
+    file_icons.insert(".mkv"," ");
+    file_icons.insert(".webm"," ");
+    file_icons.insert(".zip","󰿺 ");
+    file_icons.insert(".tar","󰛫 ");
+    file_icons.insert(".gz","󰛫 ");
+    file_icons.insert(".bz2","󰿺 ");
+    file_icons.insert(".xz","󰿺 ");
+    file_icons.insert(".jar"," ");
+    file_icons.insert(".java"," ");
+    file_icons.insert(".js"," ");
+    file_icons.insert(".json"," ");
+    file_icons.insert(".py"," ");
+    file_icons.insert(".rs"," ");
+    file_icons.insert(".yml"," ");
+    file_icons.insert(".yaml"," ");
+    file_icons.insert(".toml"," ");
+    file_icons.insert(".deb"," ");
+    file_icons.insert(".md"," ");
+    file_icons.insert(".rb"," ");
+    file_icons.insert(".php"," ");
+    file_icons.insert(".pl"," ");
+    file_icons.insert(".svg","󰜡 ");
+    file_icons.insert(".eps"," ");
+    file_icons.insert(".ps"," ");
+    file_icons.insert(".git"," ");
+    file_icons.insert(".zig"," ");
+    file_icons.insert(".xbps"," ");
+    file_icons.insert(".el"," ");
+    file_icons.insert(".vim"," ");
+    file_icons.insert(".lua"," ");
+    file_icons.insert(".pdf"," ");
+    file_icons.insert(".epub","󰂺 ");
+    file_icons.insert(".conf"," ");
+    file_icons.insert(".iso"," ");
+    file_icons.insert(".exe"," ");
+    file_icons.insert(".odt","󰷈 ");
+    file_icons.insert(".ods","󰱾 ");
+    file_icons.insert(".odp","󰈧 ");
+    file_icons.insert(".gif","󰵸 ");
+    file_icons.insert(".tiff","󰋪 ");
+    file_icons.insert(".7z"," ");
+    file_icons.insert(".bat"," ");
+    file_icons.insert(".app"," ");
+    file_icons.insert(".log"," ");
+    file_icons.insert(".sql"," ");
+    file_icons.insert(".db"," ");
+
+    file_icons
+}
+
 fn output_to_term(mut files: Vec<String>, force_col: bool, longest_file_name: usize) {
     files.sort();
-    let ncol = termsize::get().unwrap().cols / (longest_file_name as u16 + 4);
+    let ncol = termsize::get().unwrap().cols / (4 + longest_file_name as u16);
+    let nrow = files.len() as u16 / ncol;
+    let total_files = files.len();
+    println!("total files: {total_files}    nrows: {nrow}    ncol: {ncol}");
 
     let mut n = 0;
     for entry in files {
-        if force_col {
-            let split_name = &entry.split('/').collect::<Vec<&str>>();
-            if split_name.len() == 2 && split_name[1] == "" {
-                println!("{BLUE} {entry}{RESET}");
-            } else {
-                println!("{entry}");
-            }
+        // TODO print file here, check for column output after
+        let split_name = &entry.split('/').collect::<Vec<&str>>();
+        if split_name.len() == 2 && split_name[1] == "" {
+            print!("{BLUE} {entry}{RESET}");
         } else {
-            let padding = " ".repeat(4 + longest_file_name - entry.len());
-            let split_name = &entry.split('/').collect::<Vec<&str>>();
-            if split_name.len() == 2 && split_name[1] == "" {
-                print!("{BLUE} {entry}{RESET}");
-                print!("{padding}");
-            } else {
-                print!("{entry}{RESET}");
-                print!("{padding}");
-            }
-            //print!("{entry}    ");
+            // TODO: if icon isn't found append two empty space
+            print!("{entry}  ");
+        }
+        if force_col {
+            println!();
+        } else {
             n += 1;
-            if n >= ncol {
-                print!("{}", '\n');
+            if n as u16 >= ncol || entry.len() > longest_file_name {
+                println!();
                 n = 0;
+            } else {
+                let padding = " ".repeat(4 + longest_file_name - entry.len());
+                print!("{padding}");
             }
         }
     }
